798e629f0081822bb4d1dd8ed1994923a99baf9c
fandongpai <691555439@qq.com>
2017-07-01 15:47:06 +0800

    merge

---
+++ src/main/java/com/zhao/test.java
@@ -0,0 +1,23 @@
+package com.zhao;
+
+/**
+ * Created by fandongpai007 on 2017/6/30.
+ */
+public class test {
+
+    public static void main(String[] args) {
+
+        String ss = "fbajfea";
+
+        System.out.println(ss + "!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+
+        getTT();
+
+
+    }
+
+    private static void getTT(){
+        System.out.println("阿宝宝，棒棒哒！！");
+    }
+}
